cfc9f39823a902aefa7451721e94b424b475d202
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-06-05 17:07:51 +0300

    feat: add cube dynamically

--- config/express.js
+++ config/express.js
@@ -2,7 +2,8 @@ const express = require('express');
 const handlebars = require('express-handlebars');
 
 module.exports = (app) => {
-    
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
     app.engine('.hbs', handlebars({
         extname: '.hbs'
     }));
