2dbaf69d5f59128a1852555b0d36a55c2fa3eca8
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-06-10 16:09:16 +0300

    feat: migrate db.json to mongodb

--- models/cube.js
+++ models/cube.js
@@ -1,35 +1,29 @@
-const { v4 } = require('uuid');
-const { saveCube } = require('../controllers/database');
-<<<<<<< Updated upstream
-=======
+const mongoose = require('mongoose');
 
-const databaseFile = path.join(__dirname,'..', '/config/database.json');
->>>>>>> Stashed changes
+const CubeSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        maxlength: 2000
+      },
+      imageUrl: {
+        type: String,
+        required: true
+      },
+      difficulty: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 6
+      },
+      accessories: [{
+        type: 'ObjectId',
+        ref: 'Accessory'
+      }]
+})
 
-class Cube {
-    constructor(name, description, imageUrl, difficulty) {
-        this.id = v4();
-        this.name = name || "No Name";
-        this.description = description;
-        this.imageUrl = imageUrl || "placeholder";
-        this.difficulty = difficulty || 0;
-    }
-
-    save(callback) {
-        const newCube = {
-            id: this.id,
-            name: this.name,
-            description: this.description,
-            imageUrl: this.imageUrl,
-            difficulty: this.difficulty
-        }
-
-<<<<<<< Updated upstream
-        saveCube(newCube, callback);
-=======
-        saveCube(newCube);
->>>>>>> Stashed changes
-    }
-}
-
-module.exports = Cube;
+module.exports = mongoose.model('Cube', CubeSchema);
