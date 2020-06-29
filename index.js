51ad96f83d09fe5e12fc6d176dc15bb804b10e68
Boyko Katranchev <32291114+boykopk@users.noreply.github.com>
2020-06-29 10:22:48 +0300

    feat: add JWT

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
-require('dotenv').config();
-const env = process.env.NODE_ENV || 'development';
+require("dotenv").config();
+const env = process.env.NODE_ENV || "development";
 
-const mongoose = require('mongoose');
-const config = require('./config/config')[env];
-const express = require('express');
-const indexRouter = require('./routes/index');
-const authRouter = require('./routes/auth');
-const cubeRouter = require('./routes/cube');
-const accessoryRouter = require('./routes/accessory');
+const mongoose = require("mongoose");
+const config = require("./config/config")[env];
+const express = require("express");
+const indexRouter = require("./routes/index");
+const authRouter = require("./routes/auth");
+const cubeRouter = require("./routes/cube");
+const accessoryRouter = require("./routes/accessory");
 const app = express();
 
-mongoose.connect(config.databaseUrl, {
+mongoose.connect(
+  config.databaseUrl,
+  {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-    }, (err) => {
-        if (err) {
-            console.error(err);
-            throw err;
-        }
-        
-    console.log('Database is setup and running');
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  },
+  (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    }
+
+    console.log("Database is setup and running");
+  }
+);
+
+require("./config/express")(app);
+
+app.use("/", authRouter);
+app.use("/", cubeRouter);
+app.use("/", accessoryRouter);
+app.use("/", indexRouter);
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "Error | Cube Workshop",
+  });
 });
 
-require('./config/express')(app);
-
-app.use('/', authRouter);
-app.use('/', cubeRouter);
-app.use('/', accessoryRouter);
-app.use('/', indexRouter);
-
-app.get('*', (req, res) => {
-    res.render('404', {
-        title: 'Error | Cube Workshop'
-    });
-});
-
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+app.listen(
+  config.port,
+  console.log(`Listening on port ${config.port}! Now its up to you...`)
+);
